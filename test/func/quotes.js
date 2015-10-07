f21ac34a0e4ad9311ee2656a841bb45448b65ba8
fengb <contact@fengb.info>
2015-10-06 20:09:47 -0500

    Convert callback hell into coroutine fun!

--- test/func/quotes.js
+++ test/func/quotes.js
@@ -1,23 +1,14 @@
 const { request, app, Promise } = require('./support')
 
 describe('func/quotes', () => {
-  it('creates a quote', (done) => {
-    let id = null
-
-    request(app)
+  it('creates a quote', Promise.coroutine(function * () {
+    let res = yield request(app)
       .post('/quotes')
       .send({ quote: 'This is the beginning of the end' })
       .expect(200)
-      .expect((res) => {
-        id = res.body.id
-      })
-      .end((err, res) => {
-        if (err) throw err
 
-        request(app)
-          .get(`/quotes/${id}`)
-          .expect(200, { quote: 'This is the beginning of the end' })
-          .end(done)
-      })
-  })
+    yield request(app)
+      .get(`/quotes/${res.body.id}`)
+      .expect(200, { quote: 'This is the beginning of the end' })
+  }))
 })
