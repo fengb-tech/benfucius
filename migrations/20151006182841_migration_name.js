51a618e627abbe279027a28cf356d382fb967877
fengb <contact@fengb.info>
2015-10-08 21:53:27 -0500

    Removed silly await/async wrapper

--- migrations/20151006182841_migration_name.js
+++ migrations/20151006182841_migration_name.js
@@ -1,16 +1,12 @@
 module.exports = {
   up (knex, Promise) {
-    return Promise.spawn(function * () {
-      yield knex.schema.createTable('quotes', function(table) {
-        table.increments('id')
-        table.string('text')
-      })
+    return knex.schema.createTable('quotes', function(table) {
+      table.increments('id')
+      table.string('text')
     })
   },
 
   down (knex, Promise) {
-    return Promise.spawn(function * () {
-      yield knex.schema.dropTable('quotes')
-    })
+    return knex.schema.dropTable('quotes')
   },
 }
