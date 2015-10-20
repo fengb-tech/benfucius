1c9711350651205931e9922f88a1dc7c9e635e16
fengb <contact@fengb.info>
2015-10-19 20:18:17 -0500

    Replaced boilerplate with co-mocha

--- test/controllers/quotes.js
+++ test/controllers/quotes.js
@@ -1,22 +1,22 @@
-const { expect, request, app, Promise, db, resourceParse } = require('test/support')
+const { expect, request, app, db, resourceParse } = require('test/support')
 const Quote = require('lib/models/quote')
 
 describe('func/quotes', () => {
   db.sync()
 
   describe('GET :id', () => {
-    it('gets a quote', Promise.coroutine(function * () {
+    it('gets a quote', function * () {
       let quote = new Quote({ text: 'I am the walrus' })
       yield quote.save()
 
       yield request(app())
         .get(`/quotes/${quote.get('id')}`)
         .expect(200, { quote: quote.toJSON() })
-    }))
+    })
   })
 
   describe('POST', () => {
-    it('creates a quote', Promise.coroutine(function * () {
+    it('creates a quote', function * () {
       let res = yield request(app())
         .post('/quotes')
         .send({ text: 'This is the beginning of the end' })
@@ -27,6 +27,6 @@ describe('func/quotes', () => {
 
       let quote = yield Quote.where({ id }).fetch()
       expect(quote.get('text')).to.equal('This is the beginning of the end')
-    }))
+    })
   })
 })
--- test/models/quote.js
+++ test/models/quote.js
@@ -1,4 +1,4 @@
-const { expect, _, db, Promise } = require('test/support')
+const { expect, _, db } = require('test/support')
 const Quote = require('lib/models/quote')
 
 describe('Quote', () => {
@@ -17,12 +17,12 @@ describe('Quote', () => {
   describe('.fetchRandom', () => {
     db.sync()
 
-    beforeEach(Promise.coroutine(function * () {
+    beforeEach(function * () {
       let quotes = _.times(10, () => new Quote())
       yield * _.map(quotes, (q) => q.save())
-    }))
+    })
 
-    it('returns random quotes', Promise.coroutine(function * () {
+    it('returns random quotes', function * () {
       let quotes = yield * _.times(3, () => Quote.fetchRandom())
       let uniqQuoteIds = _(quotes)
                            .map((q) => q.get('id'))
@@ -30,69 +30,69 @@ describe('Quote', () => {
                            .value()
 
       expect(uniqQuoteIds).to.not.have.length(1)
-    }))
+    })
   })
 
   describe('.withVotes', () => {
     db.sync()
 
-    beforeEach(Promise.coroutine(function * () {
+    beforeEach(function * () {
       let quote = new Quote()
       this.quote = yield quote.save()
-    }))
+    })
 
     describe('#get("positive_votes")', () => {
-      it('= null by default', Promise.coroutine(function * () {
+      it('= null by default', function * () {
         let quote = yield Quote.withVotes().fetch()
         expect(quote.get('positive_votes')).to.be.null()
-      }))
+      })
 
-      it('= 0.5 with votes', Promise.coroutine(function * () {
+      it('= 0.5 with votes', function * () {
         let votes = this.quote.related('votes')
         this.plusVote = yield votes.create({ value: '+' })
         this.minusVote = yield votes.create({ value: '-' })
 
         let quote = yield Quote.withVotes().fetch()
         expect(quote.get('positive_votes')).to.eq(0.5)
-      }))
+      })
     })
   })
 
   describe('.kosher', () => {
     db.sync()
 
-    beforeEach(Promise.coroutine(function * () {
+    beforeEach(function * () {
       let quote = new Quote()
       this.quote = yield quote.save()
-    }))
+    })
 
-    it('is kosher by default', Promise.coroutine(function * () {
+    it('is kosher by default', function * () {
       let quote = yield Quote.kosher().fetch()
       expect(quote).to.not.be.null()
       expect(quote.get('id')).to.equal(this.quote.get('id'))
-    }))
+    })
 
     describe('with 50% "wtf" vote', function () {
-      beforeEach(Promise.coroutine(function * () {
+      beforeEach(function * () {
         let votes = this.quote.related('votes')
         this.plusVote = yield votes.create({ value: '+' })
         this.wtfVote = yield votes.create({ value: 'wtf' })
-      }))
+      })
 
-      it('is not kosher if over minCount and under threshold', Promise.coroutine(function * () {
+      it('is not kosher if over minCount and under threshold', function * () {
         let quote = yield Quote.kosher({ minCount: 2, threshold: 1 }).fetch()
         expect(quote).to.be.null()
-      }))
+      })
 
-      it('is kosher if under minCount', Promise.coroutine(function * () {
+      it('is kosher if under minCount', function * () {
         let quote = yield Quote.kosher({ minCount: 3, threshold: 1 }).fetch()
         expect(quote).to.not.be.null()
-      }))
+      })
 
-      it('is kosher if over threshold', Promise.coroutine(function * () {
+      it('is kosher if over threshold', function * () {
         let quote = yield Quote.kosher({ minCount: 2, threshold: 0.4 }).fetch()
         expect(quote).to.not.be.null()
-      }))
+      })
     })
   })
 })
