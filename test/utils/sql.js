275b2bd53b82d005c432541b6b5e85a7a76c32ae
fengb <contact@fengb.info>
2015-10-27 23:31:48 -0500

    Better SQL sanitation

--- test/utils/sql.js
+++ test/utils/sql.js
@@ -4,13 +4,8 @@ const sql = require('lib/utils/sql')
 describe('lib/utils/sql', () => {
   describe('.random', () => {
     it('wraps values into random-sql snippet', () => {
-      expect(sql.random(1, 10))
+      expect(sql.random(1, 10).toString())
         .to.equal('(random() * ((10) - (1)) + (1))')
-    })
-
-    it('wraps strings into random-sql snippet', () => {
-      expect(sql.random(1, 'count(*)'))
-        .to.equal('(random() * ((count(*)) - (1)) + (1))')
     })
   })
 })
