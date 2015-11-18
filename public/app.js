d8c6754bd5a3803292e47dadc6500769db14ebe2
fengb <contact@fengb.info>
2015-11-17 18:52:27 -0600

    Refactored lightbox/recaptcha behavior

--- public/app.js
+++ public/app.js
@@ -8,6 +8,16 @@ String.prototype.format = function () {
   return s
 }
 
+$.fn.serializeJSON = function () {
+  var array = this.serializeArray()
+  var json = {}
+  for (var i = 0; i < array.length; i++) {
+    var element = array[i]
+    json[element.name] = element.value
+  }
+  return json
+}
+
 function ajaxUpdateAll (data) {
   $.each(data, function (objectType, objectData) {
     var $parentE = $('[data-{0}-id="{1}"]'.format(objectType, objectData.id))
@@ -18,45 +28,83 @@ function ajaxUpdateAll (data) {
   })
 
   if (data.recaptcha && data.recaptcha.SITE_KEY) {
-    loadRecaptcha(data.recaptcha.SITE_KEY)
+    Recaptcha.load(data.recaptcha.SITE_KEY)
   }
 }
 
-function loadRecaptcha (siteKey) {
-  var $lightboxWrapper = $('<div class="lightbox-wrapper">').appendTo('body')
-  var $lightbox = $('<div class="lightbox">').appendTo($lightboxWrapper)
-  var $form = $('<form method="POST">').appendTo($lightbox)
-  $form.append('<div class="g-recaptcha" data-sitekey="{0}">'.format(siteKey))
-  $.getScript('https://www.google.com/recaptcha/api.js')
-
-  var poll = setInterval(function () {
-    var recaptchaVal = $form.find('.g-recaptcha-response').val()
-    if (recaptchaVal) {
-      clearInterval(poll)
-      setTimeout(function () {
-        $.ajax({
-          url:    '/session',
-          method: 'POST',
-          data: { 'g-recaptcha-response': recaptchaVal },
-          success: function () {
-            $lightboxWrapper.remove()
-          }
-        })
-      }, 1000)
+var Lightbox = {
+  show: function ($e, css) {
+    if (this.$lightboxWrapper) {
+      this.hide()
     }
-  }, 100)
+
+    this.$lightboxWrapper = $('<div class="lightbox-wrapper">').appendTo('body')
+    var $lightbox = $('<div class="lightbox">').appendTo(this.$lightboxWrapper)
+    if (css) {
+      $lightbox.css(css)
+    }
+    $lightbox.append($e)
+    $(document).on('keyup', function (event) {
+      if (event.keyCode === 27) {
+        Lightbox.hide()
+      }
+    })
+    this.$lightboxWrapper.on('click', function (event) {
+      if (event.target !== $lightbox[0]) {
+        Lightbox.hide()
+      }
+    })
+  },
+
+  hide: function () {
+    this.$lightboxWrapper.remove()
+    $(document).off('keypress')
+  },
+}
+
+var Recaptcha = {
+  load: function (siteKey) {
+    var $form = $('<form method="POST">')
+    $form.append('<div class="g-recaptcha" data-sitekey="{0}">'.format(siteKey))
+    Lightbox.show($form, { width: 300 })
+
+    $.getScript('https://www.google.com/recaptcha/api.js')
+
+    var poll = setInterval(function () {
+      var recaptchaVal = $form.find('.g-recaptcha-response').val()
+      if (recaptchaVal) {
+        clearInterval(poll)
+        setTimeout(function () {
+          Recaptcha.submit($form)
+        }, 1000)
+      }
+    }, 100)
+  },
+
+  submit: function ($form) {
+    $.ajax({
+      url:    '/session',
+      method: 'POST',
+      data: $form.serializeJSON(),
+      success: function () {
+        Lightbox.hide()
+      },
+      error: function (xhr) {
+        Recaptcha.load(xhr.responseJSON.recaptcha.SITE_KEY)
+      },
+    })
+  },
 }
 
 $('.vote-form').on('submit', function (event) {
   event.preventDefault()
 
   var $form = $(this)
-  var json = $form.serializeArray()[0]
 
   $.ajax({
     url:     $form.attr('action'),
     method:  $form.attr('method'),
-    data:    json,
+    data:    $form.serializeJSON(),
     success: ajaxUpdateAll,
   })
 })
